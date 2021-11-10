53bb353cd24aeb58d3d11de94ee1e777f9f75034
Hamidoo <hussainhafeez1100@gmail.com>
2021-11-10 11:47:54 +0530

    added tests

--- src/Components/__tests__/App.test.js
+++ src/Components/__tests__/App.test.js
@@ -1,37 +1,9 @@
-import React from "react";
-import { render as rtlRender, fireEvent, screen } from "@testing-library/react";
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import userReducer from "../../Features/InputSlice";
-import App from "../../App";
-import RightSideBar, { ScheduleButton } from "../RightSideBar";
-import Navbar from "../Navbar";
+import React from 'react'
+import store from "../../App/store";
+import { screen } from './test-utils';
+import Navbar from '../Navbar';
 
-function render(
-  ui,
-  {
-    preloadedState,
-    store = configureStore({ reducer: { user: userReducer }, preloadedState }),
-    ...renderOptions
-  } = {}
-) {
-  function Wrapper({ children }) {
-    return <Provider store={store}>{children}</Provider>;
-  }
-  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
-}
-
-
-
-test("App should be rendered in the document", () => {
-  <App />;
-});
-
-
-test("Rightside bar should render", () => {
-  <RightSideBar />;
-});
-
-test("Navbar bar should render", () => {
-  render(<App />);
-});
+  test("should have test Upgrade", () => {
+    let state = store.getState();
+    expect(state.getByTestId("upgrade-btn")).toBeInTheDocument()
+  })
